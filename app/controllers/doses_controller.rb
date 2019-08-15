217167b14a6ffde9ac5a050c84424637be64a0c4
Elisa <codersquirrelbln@gmail.com>
2019-08-15 16:24:41 +0200

    add dose controller

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,34 @@
 class DosesController < ApplicationController
+
+  def new
+    @dose = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @ingredient = Ingredient.all
+  end
+
+  def create
+    @dose = Dose.new(doses_params)
+    @dose.cocktail = Cocktail.find(params[:cocktail_id])
+    if @dose.save
+      redirect_to cocktail_path(@dose.cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @cocktail = @dose.cocktail
+    @dose.destroy
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  private
+
+  def doses_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
 end
+
+
+
